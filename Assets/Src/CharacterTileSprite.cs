51105c1a7fd8eb408ef034885753e4ca59bc4e25
MounirTakieddine <MounirTakieddine@bitbucket.org>
2018-08-25 13:57:42 +0100

    Modified logic inside CharacterTileSprite.ComputeVelocity() to account for player input

--- Assets/Src/CharacterTileSprite.cs
+++ Assets/Src/CharacterTileSprite.cs
@@ -1,11 +1,48 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
-using System.Linq;
 
 public class CharacterTileSprite : AgentTileSprite
 {
+    public ControllerStateMachine ControllerState { get; private set; }
+
+    public ControllerStateMachine.JoystickConfiguration JoystickConfiguration;
+    public ControllerStateMachine.KeyboardConfiguration KeyboardConfiguration;
+
+    public float SpeedX, SpeedY;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        ControllerState = new ControllerStateMachine();
+    }
+
     public override void ComputeVelocity()
     {
-        // TODO!
+        ControllerState.Monitor(JoystickConfiguration, KeyboardConfiguration);
+        
+        if (ControllerState.IsMovingLeft())
+        {
+            VelocityX = -SpeedX;
+        }
+        else if (ControllerState.IsMovingRight())
+        {
+            VelocityX = SpeedX;
+        }
+        else
+        {
+            VelocityX = 0.0f;
+        }
+
+        if (ControllerState.IsMovingUp())
+        {
+            VelocityY = SpeedY;
+        }
+        else if (ControllerState.IsMovingDown())
+        {
+            VelocityY = -SpeedY;
+        }
+        else
+        {
+            VelocityY = 0.0f;
+        }
     }
 };
