8253455acec2276e86682d343ebd282fb3291efd
MounirTakieddine <MounirTakieddine@bitbucket.org>
2018-09-01 20:18:46 +0100

    Applied singleton design pattern in creating BoundingBoxManager class controlling movement and collision between ActiveBoundingBox objects

---
+++ Assets/Src/BoundingBoxManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BoundingBoxManager : MonoBehaviour
+{
+    public static BoundingBoxManager Instance { get; private set; }
+   
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (!Instance != this)
+        {
+            Destroy(this);
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+
+    }
+    
+    void Update ()
+    {
+		
+	}
+};
